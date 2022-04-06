238bb5780b36c95f058f7ac40493ae06ad0ee87d
donus <donus33@gmail.com>
2022-04-06 11:19:18 +0700

    initial

---
+++ rules.mk
@@ -0,0 +1,8 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+UNICODE_ENABLE = yes         # Unicode
