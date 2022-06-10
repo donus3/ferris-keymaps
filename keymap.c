b9dbf8f8b5e6892d912e117a5877603505c6472f
Pongpayak <pongpayak@PONGPAYAK-BOML7H.local>
2022-06-10 10:06:18 +0700

    update layout

--- keymap.c
+++ keymap.c
@@ -9,11 +9,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT( // numbers and symbols
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_QUOT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_CAPS, KC_GRV,  KC_BSLS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_CAPS,                      KC_TRNS, KC_GRV,  KC_BSLS, KC_TRNS, KC_TRNS,
                                KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS),
   [2] = LAYOUT( // functions
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_TAB,  KC_VOLD, KC_VOLU, KC_ESC,  KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,
+    KC_TAB,  KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,
     KC_MUTE, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
                                KC_TAB,  KC_ESC,                       KC_TRNS, KC_TRNS),
   [3] = LAYOUT( // navigation
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS),
   [4] = LAYOUT( // mouse
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_BTN3,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                               KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS)
+                               KC_BTN1, KC_BTN2,                      KC_TRNS, KC_TRNS)
 };
