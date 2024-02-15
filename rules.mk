d5b43e1b7e08ee4c176c24ecfe91f179befb7286
Mr.Pongpayak <donus33@gmail.com>
2024-02-15 22:09:19 +0700

    enable via

--- rules.mk
+++ rules.mk
@@ -3,6 +3,7 @@
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes        # Mouse keys
-EXTRAKEY_ENABLE = yes        # Audio control and System control
-UNICODE_ENABLE = yes         # Unicode
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+UNICODE_ENABLE = yes        # Unicode
+VIA_ENABLE = yes            # Enable via: https://www.caniusevia.com/docs/configuring_qmk
