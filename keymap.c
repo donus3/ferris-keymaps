041ccf36a518af798c3881a208be8cfc06b2ae6a
donus <donus33@gmail.com>
2025-03-02 17:04:57 +0700

    update keymap

--- keymap.c
+++ keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM esc_combo[] = {RGUI_T(KC_J), RALT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM esc_combo[] = {LGUI_T(KC_J), LALT_T(KC_K), COMBO_END};
 combo_t key_combos[] = {
     COMBO(esc_combo, KC_ESC)
 };
@@ -8,7 +8,7 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-   LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LGUI_T(KC_F),KC_G,          KC_H,RGUI_T(KC_J),RALT_T(KC_K),RCTL_T(KC_L),RSFT_T(KC_SCLN),
+   LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LGUI_T(KC_F),KC_G,          KC_H,LGUI_T(KC_J),LALT_T(KC_K),LCTL_T(KC_L),LSFT_T(KC_SCLN),
    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                               MO(1),   LT(3,KC_SPC),                  LT(4,KC_ENT),     LT(2,KC_BSPC)),
   [1] = LAYOUT( // numbers and symbols
